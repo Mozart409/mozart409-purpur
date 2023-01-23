13ace977bc2baec7dfd30ed6b19bce41a8186456
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-01-23 12:47:19 +0100

    feat: :zap: added simd to startup

--- templates/server.sh
+++ templates/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Start the purpur.jar server with max 2gb of ram
-java -Xmx2G -jar purpur.jar nogui
+/usr/bin/java --add-modules=jdk.incubator.vector -Xms1G -Xmx2G -jar paper.jar --nogui
